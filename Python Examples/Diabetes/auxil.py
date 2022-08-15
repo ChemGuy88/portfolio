19442f486279e8ce7a64df9731f76dfa5d16bdb1
Herman <hf.autore@gmail.com>
2022-08-15 13:56:39 -0400

    update

--- Python Examples/Diabetes/auxil.py
+++ Python Examples/Diabetes/auxil.py
@@ -1,30 +1,20 @@
 """
 Helper file
 """
-import numpy as np
-import matplotlib.pyplot as plt
+# https://stackoverflow.com/questions/8971834/matplotlib-savefig-with-a-legend-outside-the-plot
+import matplotlib.pyplot as pyplot
 
-no_samples = 8000
-x = np.linspace(0, no_samples, 160)
-no_labels = int(np.floor(len(x) / 20))
-label = [f'{i * no_samples / no_labels:.0f}' for i in range(no_labels+1)]
+x = [0, 1, 2, 3, 4]
+y = [xx*xx for xx in x]
 
-fig = plt.figure(figsize=(10, 5))
-ax = fig.add_axes([0.1, 0.1, 0.85, 0.85])
+fig = pyplot.figure()
+ax  = fig.add_subplot(111)
 
-N = 100
-data = np.random.normal(np.tile(100 / (x+1000), N), 0.001).reshape(N, -1)
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width*0.8, box.height])
 
-flyprops = {'markersize':0.01}
-colorprops = None
-ax.boxplot(data, flierprops=flyprops, showcaps=False,
-           boxprops=colorprops, whiskerprops={'color': 'tab:blue'},
-           patch_artist=True)
+ax.plot(x, y)
+leg = ax.legend(['abc'], loc = 'center left', bbox_to_anchor = (1.0, 0.5))
+#pyplot.show()
 
-ax.set_xlabel("Samples", labelpad=10)
-ax.set_ylabel("Error (MSE)", labelpad=10)
-ax.set_ylim(0, 0.11)
-ax.set_xticks(range(0, len(x)+1, 20))
-ax.set_xticklabels(label)
-
-plt.show()
+fig.savefig('aaa.png', bbox_inches='tight')
